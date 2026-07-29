4cc64ac3e7ca6ddc2f8d5f3860392ece838ff9f6
Renato Arredondo <renato.avela.17@gmail.com>
2026-07-28 19:46:50 -0500

    fix: remove unnecessary error check for VITE_FILE_BASE_URL in env configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,10 +6,6 @@ type Env = {
 
 const _env = import.meta.env as unknown as Env
 
-if(!_env.VITE_FILE_BASE_URL) {
-  throw new Error('El VITE_FILE_BASE_URL es necesario')
-}
-
 export const env = {
   apiUrl: _env.VITE_API_URL,
   fileBaseUrl: _env.VITE_FILE_BASE_URL,
